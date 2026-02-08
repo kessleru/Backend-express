a5ddd5baab1f615ca44835602ea32e92e7def9ec
kessleru <otaviokesslerustra@hotmail.com>
2026-02-08 11:56:00 -0300

    feat: enhance Express server with improved JSON handling and detailed comments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,29 @@ const express = require('express');
 
 const app = express();
 
+// Para o express entender o formato JSON
+app.use(express.json());
+
+/**
+ * GET - Buscar uma informação dentro do servidor
+ * POST - Inserir uma informação no servidor
+ * PUT - Alterar uma informação no servidor
+ * PATCH - Alterar uma informação específica
+ * DELETE - Deletar uma informação no servidor
+ */
+
+/**
+ * Tipos de parametros
+ * 
+ * Route Params => Identificar um recurso editar/deletar/buscar | Obrigatório
+ * Query Params => Paginação / Filtro | Opcional
+ * Request Body => Conteúdo na hora de criar ou editar um recurso (JSON) | Obrigatório
+ */
+
 app.get('/', (request, response) => {
+  // Exemplo: http://localhost:5050/?name=Ignite&idade=2026
+  const query = request.query;
+  console.log(query);
   return response.json({ message: 'Hello World Ignite!' });
 });
 
@@ -11,15 +33,21 @@ app.get('/courses', (request, response) => {
 });
 
 app.post('/courses', (request, response) => {
-  return response.json(['curso 1', 'curso 2', 'curso 3'], ['curso 4']);
+  const body = request.body;
+  console.log(body);
+  return response.json(['curso 1', 'curso 2', 'curso 3', 'curso 4']);
 });
 
 app.put('/courses/:id', (request, response) => {
-  return response.json(['curso 6', 'curso 2', 'curso 3'], ['curso 4']);
+  // const { id } = request.params
+  // console.log(id)
+  const params = request.params;
+  console.log(params);
+  return response.json(['curso 6', 'curso 2', 'curso 3', 'curso 4']);
 });
 
 app.patch('/courses/:id', (request, response) => {
-  return response.json(['curso 6', 'curso 7', 'curso 3'], ['curso 4']);
+  return response.json(['curso 6', 'curso 7', 'curso 3', 'curso 4']);
 });
 
 app.delete('/courses/:id', (request, response) => {
